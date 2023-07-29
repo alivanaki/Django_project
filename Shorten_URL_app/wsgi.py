c377b06a8e7cc5cb1bd7c4f790e183949944df18
Ali <Ali@gmail.com>
2023-07-29 14:25:20 +0330

    Change name of project.

--- Shorten_URL_app/wsgi.py
+++ Shorten_URL_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for new_project project.
+WSGI config for Shorten_URL_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shorten_URL_app.settings')
 
 application = get_wsgi_application()
